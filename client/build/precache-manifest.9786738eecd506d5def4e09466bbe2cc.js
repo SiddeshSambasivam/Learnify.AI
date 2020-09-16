3cb6cc7bd4e2fab88fe5999ec568eef2bcc8ef20
Yuxiang Ladlady <yuxiangladlady@Yuxiangs-MacBook-Air.local>
2020-09-16 15:57:44 +0800

    change top part

---
+++ client/build/precache-manifest.9786738eecd506d5def4e09466bbe2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ddddc26dbca40bc16d23a2b485f942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdf3d101674fb3597720",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fe0e4d7047ccd3f582c",
+    "url": "/static/js/2.ff1a05cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff1a05cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf3d101674fb3597720",
+    "url": "/static/js/main.b187578b.chunk.js"
+  },
+  {
+    "revision": "7bb492c09db118f4ff1b",
+    "url": "/static/js/runtime-main.5b35c587.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React, { Component } from 'react';
 
+
 class App extends Component {
   state = {
     render: false
   }
-
+  
   handleClick = () => {
     this.setState({render:true})
   }
@@ -13,27 +14,46 @@ class App extends Component {
     if(this.state.render === false){
     return (  <React.Fragment>
       <form>
+  
+       <div class="centered">
+       <h1>Node mind</h1>
+       </div>
+
+        <h2> NTU EEE: Group 57</h2>
+   
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
+        <label for="exampleInputEmail1">Student ID</label>
         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        <small id="emailHelp" class="form-text text-muted">Please share with me your password dumbfck</small>
-      </div>
-      <div class="form-group">
+        <small id="emailHelp" class="form-text text-muted"> No Spacing Needed </small>
+       </div>
+
+       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1"/>
-      </div>
-      <div class="form-group form-check">
+       </div>
+       <div><select>
+<option value="Year 4">Year 4</option>
+<option value="Year 3">Year 3</option>
+<option value="Year 2">Year 2</option>
+<option value="Year 2">Year 1</option>
+<option selected value="Year of study">Year of study</option>
+</select>
+</div>
+       <div class="form-group form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
+        <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+       </div>
+
       <button type="submit" class="btn btn-primary" onClick={this.handleClick}>Submit</button>
     </form>
     </React.Fragment>);
+   
     }
-    return (<React.Fragment><h1>Hello world</h1></React.Fragment>)
+    return (<React.Fragment><h1>Do Not Cheat The System</h1></React.Fragment>)
   }
 
 }
- 
+
 export default App;
+
 
